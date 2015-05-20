03b267742fe0781c94ef7591f4ee4cac18aa4d18
pizza-pan <patrickpanzz17@gmail.com>
2015-05-19 21:08:13 -0400

    add help icon

---
+++ languages/en.js
@@ -0,0 +1,8 @@
+window.angularTranslationsLoaded('en',{
+"RULES_OF_GO":"New to Connect5?",
+"RULES_SLIDE1":"It is a pretty simply game where players take their turns one by one.",
+"RULES_SLIDE2":"Rule 1 states that every stone should be placed on an empty cell",
+"RULES_SLIDE3":"Rule 2 states that whoever has his/her 5 pieces to connect in a line first wins the game.",
+"RULES_SLIDE4":"Think harder before you act. Only by combining defence and offence can you win the game.",
+"CLOSE":"Close"
+});
---
+++ languages/zh.js
@@ -0,0 +1,8 @@
+window.angularTranslationsLoaded('zh',{
+"RULES_OF_GO":"第一次玩五子棋？",
+"RULES_SLIDE1":"五子棋的规则非常简单,双方轮流下棋",
+"RULES_SLIDE2":"规则1. 你必须点击一个没有被占领的格子下棋",
+"RULES_SLIDE3":"规则2. 最先把本方五个棋子连成一线的玩家获得胜利",
+"RULES_SLIDE4":"开动脑筋, 攻守兼备才能赢得比赛",
+"CLOSE":"关闭"
+});
